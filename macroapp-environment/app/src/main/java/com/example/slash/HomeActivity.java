7564368353403da15f36fcf9efe2a4d9716c5ac9
PrimordiaGod <104737279+PrimordiaGod@users.noreply.github.com>
2025-02-28 19:01:36 +0800

    Update HomeActivity.java

--- macroapp-environment/app/src/main/java/com/example/slash/HomeActivity.java
+++ macroapp-environment/app/src/main/java/com/example/slash/HomeActivity.java
@@ -4,13 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
-
     private FirebaseAuth mAuth;
 
     @Override
